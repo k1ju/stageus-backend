9230d797b33aa4dc50569bbb7f4d3d3156d479ce
k1ju <gksrlwn98@naver.com>
2024-01-18 16:07:11 +0900

    feat: add env.js

---
+++ src/config/env.js
@@ -0,0 +1,11 @@
+const env = {
+    HTTP_PORT: process.env.HTTP_PORT,
+
+    PSQL_USER: process.env.PSQL_USER,
+    PSQL_DATABASE: process.env.PSQL_DATABASE,
+    PSQL_PORT: process.env.PSQL_PORT,
+    PSQL_HOST: process.env.PSQL_HOST,
+    PSQL_PW: process.env.PSQL_PW,
+};
+
+module.exports = env;
