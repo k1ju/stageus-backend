9204e320729299a864c6dc2ceef5e792e4030b99
k1ju <gksrlwn98@naver.com>
2024-01-18 16:07:28 +0900

    feat: applye env.js into server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const app = require('./src/app');
+const env = require('./src/config/env');
 const { mongoDB } = require('./src/config/mongodb');
 
 const main = async () => {
-
-    app.listen(process.env.port, '0.0.0.0', () => {
-        console.log(`${process.env.port}번 포트번호 서버실행`);
+    app.listen(env.HTTP_PORT, '0.0.0.0', () => {
+        console.log(`${env.HTTP_PORT}번 포트번호 서버실행`);
     });
 };
 
 main();
-
-
-
